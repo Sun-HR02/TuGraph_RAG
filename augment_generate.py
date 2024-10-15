b53d7530f1e3f8bb9f3093eb2ac4968582a05150
sunhaoran.hte@foxmail.com <1021130731@qq.com>
2024-10-15 14:02:46 +0800

    update augment_generate.py

--- augment_generate.py
+++ augment_generate.py
@@ -1,6 +1,8 @@
 import json
 from openai import OpenAI
 from tqdm import tqdm
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
 
 
 num=0
@@ -39,13 +41,24 @@ def knowledge2str(knowledge, tokens_per_knowledge):
         knowledgeStr +=  document.page_content + ' '
     return knowledgeStr
 
+def get_response_from_llm(prompt, options):
+    model_path = options['chat-model']
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    model = AutoModelForCausalLM.from_pretrained(model_path)
+    inputs = tokenizer(prompt, return_tensors="pt")
+    generate_ids = model.generate(inputs.input_ids, max_length=30)
+    return tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
 
 def generate_answer(query, knowledge, options):  
     prompt = ""
     prompt = query
+    is_api = options['is_api']
     # 修改知识添加
     prompt += '以下是辅助文本：'
     prompt += knowledge2str(knowledge,options['tokens_per_knowledge'] )
+    if is_api:
+        response = get_gpt_response_w_system(prompt, options)
+    else :
+        response = get_response_from_llm(prompt,options)
 
-    response = get_gpt_response_w_system(prompt, options)
     return response
